84de44bd33f1ba55df0b56a4ab1af66b5b247bec
Rayhan Akond Hridoy <rayhan.hridoy@alaricsecurities.com>
2022-11-05 19:59:22 +0600

    A. Add Odd or Subtract Even - 1311A added

---
+++ A. Add Odd or Subtract Even-Problem-1311A/A. Add Odd or Subtract Even-Problem-1311A/Program.cs
@@ -0,0 +1,24 @@
+﻿int t = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < t; i++)
+{
+    string[] ab = Console.ReadLine().Split();
+
+    int a = int.Parse(ab[0]);
+    int b = int.Parse(ab[1]);
+    int s;
+
+    if (a > b)
+    {
+        if ((a - b) % 2 == 0) s = 1;
+        else s = 2;
+    }
+    else if (a < b)
+    {
+        if ((a - b) % 2 == 0) s = 2;
+        else s = 1;
+    }
+    else s = 0;
+
+    Console.WriteLine(s);
+}
